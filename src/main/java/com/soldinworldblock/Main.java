a8e08889e194553dcf11bab74027816edac44899
Soldn <kyryll524@gmail.com>
2025-08-22 21:34:09 +0100

    Update Main.java

--- src/main/java/com/soldinworldblock/Main.java
+++ src/main/java/com/soldinworldblock/Main.java
@@ -1,33 +1,26 @@
 package com.soldinworldblock;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 import com.soldinworldblock.commands.SoldinWorldBlockCommand;
-import com.soldinworldblock.listeners.PortalListener;
+import com.soldinworldblock.listeners.PortalAndTeleportListener;
 import com.soldinworldblock.managers.WorldBlockManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-    private static Main instance;
     private WorldBlockManager worldBlockManager;
 
     @Override
     public void onEnable() {
-        instance = this;
         saveDefaultConfig();
+
         worldBlockManager = new WorldBlockManager(this);
+
+        // Команда
         getCommand("soldinworldblock").setExecutor(new SoldinWorldBlockCommand(this));
-        Bukkit.getPluginManager().registerEvents(new PortalListener(this), this);
-        getLogger().info("SoldinWorldBlock успешно запущен!");
-    }
+        getCommand("soldinworldblock").setTabCompleter(new SoldinWorldBlockCommand(this));
 
-    @Override
-    public void onDisable() {
-        getLogger().info("SoldinWorldBlock отключен.");
-    }
-
-    public static Main getInstance() {
-        return instance;
+        // Слушатели
+        getServer().getPluginManager().registerEvents(new PortalAndTeleportListener(this), this);
     }
 
     public WorldBlockManager getWorldBlockManager() {
