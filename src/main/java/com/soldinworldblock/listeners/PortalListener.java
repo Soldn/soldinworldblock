04120139ee3e78631fd989e05f19b2628ac534fe
Soldn <kyryll524@gmail.com>
2025-08-22 21:31:21 +0100

    Update PortalListener.java

--- src/main/java/com/soldinworldblock/listeners/PortalListener.java
+++ src/main/java/com/soldinworldblock/listeners/PortalListener.java
@@ -1,40 +1,55 @@
 package com.soldinworldblock.listeners;
 
 import com.soldinworldblock.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPortalEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.Location;
 
-public class PortalListener implements Listener {
+public class PortalAndTeleportListener implements Listener {
 
     private final Main plugin;
 
-    public PortalListener(Main plugin) {
+    public PortalAndTeleportListener(Main plugin) {
         this.plugin = plugin;
+    }
+
+    private void blockAccess(Player player, String world) {
+        // Телепортируем игрока в безопасный мир
+        Location spawn = Bukkit.getWorld("world").getSpawnLocation();
+        player.teleport(spawn);
+        player.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_access"));
     }
 
     @EventHandler
     public void onPortal(PlayerPortalEvent event) {
         if (event.getTo() == null) return;
         Player player = event.getPlayer();
-        String targetWorld = event.getTo().getWorld().getName();
-        if (plugin.getWorldBlockManager().isBlocked(targetWorld)) {
+        String world = event.getTo().getWorld().getName();
+        if (plugin.getWorldBlockManager().isBlocked(world)) {
             event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_portal"));
+            blockAccess(player, world);
         }
     }
 
     @EventHandler
     public void onTeleport(PlayerTeleportEvent event) {
         if (event.getTo() == null) return;
-        String targetWorld = event.getTo().getWorld().getName();
-        if (plugin.getWorldBlockManager().isBlocked(targetWorld)) {
+        String world = event.getTo().getWorld().getName();
+        if (plugin.getWorldBlockManager().isBlocked(world)) {
             event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_portal"));
+            blockAccess(event.getPlayer(), world);
+        }
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        String world = event.getPlayer().getWorld().getName();
+        if (plugin.getWorldBlockManager().isBlocked(world)) {
+            blockAccess(event.getPlayer(), world);
         }
     }
 
@@ -42,11 +57,11 @@ public class PortalListener implements Listener {
     public void onCommand(PlayerCommandPreprocessEvent event) {
         String msg = event.getMessage().toLowerCase();
         Player player = event.getPlayer();
-        for (String world : plugin.getWorldBlockManager().getBlockedWorlds().keySet()) {
-            if (msg.contains("/tp") && msg.contains(world.toLowerCase())) {
+        plugin.getWorldBlockManager().getBlockedWorlds().keySet().forEach(world -> {
+            if (msg.contains(world)) {
                 event.setCancelled(true);
-                player.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_portal"));
+                player.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_access"));
             }
-        }
+        });
     }
 }
