9d4b750dd6c0f9785bc776596e2183981b2123ca
Soldn <kyryll524@gmail.com>
2025-08-22 21:33:47 +0100

    Create PortalAndTeleportListener.java

---
+++ src/main/java/com/soldinworldblock/listeners/PortalAndTeleportListener.java
@@ -0,0 +1,66 @@
+package com.soldinworldblock.listeners;
+
+import com.soldinworldblock.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+
+public class PortalAndTeleportListener implements Listener {
+
+    private final Main plugin;
+
+    public PortalAndTeleportListener(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    private void blockAccess(Player player, String world) {
+        Location spawn = Bukkit.getWorld("world").getSpawnLocation();
+        player.teleport(spawn);
+        player.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_access"));
+    }
+
+    @EventHandler
+    public void onPortal(PlayerPortalEvent event) {
+        if (event.getTo() == null) return;
+        Player player = event.getPlayer();
+        String world = event.getTo().getWorld().getName();
+        if (plugin.getWorldBlockManager().isBlocked(world)) {
+            event.setCancelled(true);
+            blockAccess(player, world);
+        }
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        if (event.getTo() == null) return;
+        String world = event.getTo().getWorld().getName();
+        if (plugin.getWorldBlockManager().isBlocked(world)) {
+            event.setCancelled(true);
+            blockAccess(event.getPlayer(), world);
+        }
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        String world = event.getPlayer().getWorld().getName();
+        if (plugin.getWorldBlockManager().isBlocked(world)) {
+            blockAccess(event.getPlayer(), world);
+        }
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        String msg = event.getMessage().toLowerCase();
+        Player player = event.getPlayer();
+        plugin.getWorldBlockManager().getBlockedWorlds().keySet().forEach(world -> {
+            if (msg.contains(world)) {
+                event.setCancelled(true);
+                player.sendMessage(ChatColor.RED + plugin.getConfig().getString("messages.blocked_access"));
+            }
+        });
+    }
+}
